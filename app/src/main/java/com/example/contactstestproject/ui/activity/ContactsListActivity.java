f07c071e97c30777d7333864c73f30134d6188d4
AradHesadi <arad1378@gmail.com>
2021-02-17 10:11:55 +0330

    app center api used

--- app/src/main/java/com/example/contactstestproject/ui/activity/ContactsListActivity.java
+++ app/src/main/java/com/example/contactstestproject/ui/activity/ContactsListActivity.java
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment;
 
 import com.example.contactstestproject.ui.fragment.ContactsListFragment;
 import com.example.contactstestproject.utils.contacts.ContactPermissionUtils;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class ContactsListActivity extends SingleFragmentActivity {
 
@@ -16,8 +19,13 @@ public class ContactsListActivity extends SingleFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         ContactPermissionUtils.requestPermission(this);
+
+        AppCenter.start(getApplication(), "20b55e59-360b-47b7-b153-95887833cc77",
+                Analytics.class, Crashes.class);
+        Crashes.generateTestCrash();
     }
 }
 
--- app/src/main/java/com/example/contactstestproject/ui/activity/SingleFragmentActivity.java
+++ app/src/main/java/com/example/contactstestproject/ui/activity/SingleFragmentActivity.java
@@ -6,6 +6,9 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
 import com.example.contactstestproject.R;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
@@ -16,7 +19,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_fragment);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+       FragmentManager fragmentManager = getSupportFragmentManager();
 
         Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_holder);
         if (fragment == null) {
